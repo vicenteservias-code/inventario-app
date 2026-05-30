399947100ece73275a802c34801901657c11bdeb
Vicente <vicente.servia.s@gmail.com>
2026-05-30 14:19:57 +0200

    nueva app completa

---
+++ public/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
