0d7de3950208559173c0f9afe7772d479e4963f0
Vicente <vicente.servia.s@gmail.com>
2026-06-04 14:10:33 +0200

    fix cache sw

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,17 @@
-const CACHE = 'inventario-v1';
+const CACHE = 'inventario-v3';
 const ARCHIVOS = ['/', '/index.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ARCHIVOS))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
